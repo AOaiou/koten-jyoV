bb130db68ca10d534df00505e3d33e01a38da911
AOaiou <74457486+AOaiou@users.noreply.github.com>
2020-12-22 17:51:38 +0900

    Create style.css

---
+++ style.css
@@ -0,0 +1,43 @@
+body{display: flex;}
+#scroll{
+    height: 100%;
+    width: 20%;
+    overflow-y: scroll;
+    padding-right: 20px;
+}
+#scroll.deco::-webkit-scrollbar{width: 15px;}
+#scroll.deco::-webkit-scrollbar-track{
+    border-radius: 10px;
+    background: skyblue;
+}
+#main{
+    width: 70%;
+    height: 100%;
+}
+ul{
+    display: flex;
+    flex-direction: column;
+}
+button{
+    border: 0;
+    height: 30px;
+}
+button:nth-child(odd){background: pink;}
+button:nth-child(even){background: lightpink;}
+table{
+    width: 100%;
+    border-collapse:collapse;
+    margin-left: auto;
+    margin-right: auto;
+}
+td, th{
+    padding: 0 auto;
+    width: 25%;
+    height: 30px;
+    border: 1;
+    
+}
+.hid{
+    color: gray;
+    background-color: gray;
+}
